d00670e44173e4ff446afac7e6c60e776f403664
VoicelessProximity1701 <praveetminash@gmail.com>
2021-12-05 12:03:43 +0000

    fgfg

--- buttonstest.py
+++ buttonstest.py
@@ -1,14 +1,11 @@
-import RPi.GPIO as GPIO
-def button_callback(channel):
-    print('Next letter')
-def button_callback2(channel):
-    print('Prev. letter')
-    
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(14,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(15,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(14,GPIO.RISING,callback=button_callback)
-GPIO.add_event_detect(15,GPIO.RISING,callback=button_callback2)
-
-
+while True:
+    textin = input('Enter word: ')
+    lengthoftextin = len(textin)
+    count1 = 0
+    for i in range(0, lengthoftextin):
+        letter = textin[i]
+        checkletnum = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        corresponding_braille = ['100000', '101000', '110000', '110100', '100100', '111000', '111100', '101100', '011000', '011100', '100010', '101010', '110010', '110110', '100110', '111010', '111110', '101110', '011010', '011110', '100011', '101011', '011101', '110011', '110111', '100111']
+        for i in range(0, 25):
+            if letter.lower() == checkletnum[i]:
+                    print('Corresponding braille: ' + corresponding_braille[i])
