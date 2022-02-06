4b817798fafa29b397b8305ebe3b51cea001d230
VoicelessProximity1701 <praveetminash@gmail.com>
2022-02-06 17:38:17 +0000

    added comments

--- appwithpinouts.py
+++ appwithpinouts.py
@@ -1,37 +1,37 @@
-from EmulatorGUI import GPIO  
-import time
+import RPi.GPIO as GPIO     # import GPIO library to allow control of GPIOs
+import time              # import the time library for pauses
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(2, GPIO.OUT)
-GPIO.setup(3, GPIO.OUT)
-GPIO.setup(4, GPIO.OUT)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)     #
+GPIO.setwarnings(False)    #
+GPIO.setup(2, GPIO.OUT)    #
+GPIO.setup(3, GPIO.OUT)    #
+GPIO.setup(4, GPIO.OUT)    # CONFIGURE GPIO PINS
+GPIO.setup(5, GPIO.OUT)    #
+GPIO.setup(6, GPIO.OUT)    #
+GPIO.setup(7, GPIO.OUT)    #
 
-textin = input('Enter word: ')
-lengthoftextin = len(textin)
-c = 0
+textin = input('Enter word: ')    # take an input from the user to enter a word
+lengthoftextin = len(textin)        # determine the length of the input string
+c = 0    # initialise a counter variable to use as a stop in a loop
 while c < lengthoftextin:
-    letter = textin[c]
-    checkletnum = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    corresponding_braille = ['100000', '101000', '110000', '110100', '100100', '111000', '111100', '101100', '011000', '011100', '100010', '101010', '110010', '110110', '100110', '111010', '111110', '101110', '011010', '011110', '100011', '101011', '011101', '110011', '110111', '100111']
-    for i in range(26):
-        if letter.lower() == checkletnum[i]:
-            temporary_letter = corresponding_braille[i]
-            for x in range(6):
-                int_temp_letter = int(temporary_letter[x])
-                GPIO.output(int(x+2), int_temp_letter)
-                time.sleep(3)
-            print('Letter = '+ checkletnum[i] + '; Braille: '+ corresponding_braille[i])
-            c = c + 1
+    letter = textin[c]    # assign the variable 'letter' to the position given by the 'c' counter variable
+    checkletnum = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']     # create an array with the letters of the alphabet
+    corresponding_braille = ['100000', '101000', '110000', '110100', '100100', '111000', '111100', '101100', '011000', '011100', '100010', '101010', '110010', '110110', '100110', '111010', '111110', '101110', '011010', '011110', '100011', '101011', '011101', '110011', '110111', '100111']  # create a corresponding array with the braille equivalents of the letters in the alphabet
+    for i in range(26):    # initialise a loop to go through all the letters of the alphabet
+        if letter.lower() == checkletnum[i]:   # when the character stored in letter is found...
+            temporary_letter = corresponding_braille[i]  # assign a variable to hold the corresponding braille value
+            for x in range(6):  # go through all GPIO pins
+                int_temp_letter = int(temporary_letter[x])  # assign a variable to convert each character in the braille value to an integer
+                GPIO.output(int(x+2), int_temp_letter) # output the GPIO value
+                time.sleep(3)    # pause for 3 seconds
+            print('Letter = '+ checkletnum[i] + '; Braille: '+ corresponding_braille[i])   # print the results to terminal
+            c = c + 1   # change the counter by 1
         if c > lengthoftextin:
-            break
+            break     # if the last letter of the word inputted has been represented, stop the program
 
 GPIO.output(2, 0)
 GPIO.output(3, 0)
-GPIO.output(4, 0)
+GPIO.output(4, 0)   # CLEAR THE PINS
 GPIO.output(5, 0)
 GPIO.output(6, 0)
 GPIO.output(7, 0)
